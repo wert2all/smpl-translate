45c0ddac9cb4dd3910daa45c76b15032a553b91d
Serhii Shvets <wert2all@gmail.com>
2025-08-12 23:30:39 +0300

    feat: create main layout

---
+++ src/app/translate-container/translate-container.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-translate-container',
+  templateUrl: './translate-container.component.html',
+  styleUrls: ['./translate-container.component.scss'],
+})
+export class TranslateContainerComponent {}
