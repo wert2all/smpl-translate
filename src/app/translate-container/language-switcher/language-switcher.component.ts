90ecd54b7c95678ac9c2b2419da8cd24ac521cf3
Serhii Shvets <wert2all@gmail.com>
2025-08-13 01:15:35 +0300

    feat: create a icon button component

--- src/app/translate-container/language-switcher/language-switcher.component.ts
+++ src/app/translate-container/language-switcher/language-switcher.component.ts
@@ -2,14 +2,15 @@ import { Component } from '@angular/core';
 import { NgIconComponent, provideIcons } from '@ng-icons/core';
 import { flagGbSquare, flagUaSquare } from '@ng-icons/flag-icons/square';
 import { phosphorArrowsLeftRightLight } from '@ng-icons/phosphor-icons/light';
+import { IconButtonComponent } from '../../shared/buttons/icon-button/icon-button.component';
 
 @Component({
   selector: 'app-language-switcher',
   templateUrl: './language-switcher.component.html',
   styleUrl: './language-switcher.component.scss',
-  imports: [NgIconComponent],
+  imports: [NgIconComponent, IconButtonComponent],
   viewProviders: [
     provideIcons({ phosphorArrowsLeftRightLight, flagUaSquare, flagGbSquare }),
   ],
 })
-export class LanguageSwitcherComponent { }
+export class LanguageSwitcherComponent {}
