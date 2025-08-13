5e5045a98b8b1f3a45dfbd611523d7e955c6b7e3
Serhii Shvets <wert2all@gmail.com>
2025-08-13 13:19:28 +0300

    feat: add state of translation

--- src/app/shared/shared.types.ts
+++ src/app/shared/shared.types.ts
@@ -7,3 +7,20 @@ export interface TextAreaSize {
   width: number;
   height: number;
 }
+
+export type State<T = unknown> =
+  | { type: 'initial' }
+  | { type: 'loading' }
+  | { type: 'failure'; error: Error }
+  | { type: 'success'; data: T };
+
+export const createInitialState = (): State => ({ type: 'initial' });
+export const createLoadingState = (): State => ({ type: 'loading' });
+export const createFailureState = (error: Error): State => ({
+  type: 'failure',
+  error,
+});
+export const createSuccessState = <T>(data: T): State<T> => ({
+  type: 'success',
+  data,
+});
