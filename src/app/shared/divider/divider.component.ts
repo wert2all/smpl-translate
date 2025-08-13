7b44c4f9a3572a977109b7ce2efa3c039dc77187
Serhii Shvets <wert2all@gmail.com>
2025-08-13 11:09:49 +0300

    feat: update translation component

---
+++ src/app/shared/divider/divider.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shared-divider',
+  template: `<div class="border-b border-gray-500"></div>`,
+})
+export class DividerComponent {}
