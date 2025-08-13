3a95b7a2b71ffecb278203ebcb73b3264ffcb393
Serhii Shvets <wert2all@gmail.com>
2025-08-13 11:43:59 +0300

    feat: update translation container height on change textarea size

--- src/app/translate-container/translation/translation.component.ts
+++ src/app/translate-container/translation/translation.component.ts
@@ -1,4 +1,4 @@
-import { Component, input } from '@angular/core';
+import { Component, computed, input } from '@angular/core';
 import { NgIcon, provideIcons } from '@ng-icons/core';
 import { phosphorTranslateLight } from '@ng-icons/phosphor-icons/light';
 import { DividerComponent } from '../../shared/divider/divider.component';
@@ -14,4 +14,9 @@ import { TitleComponent } from '../../shared/title/title.component';
 })
 export class TranslationComponent {
   translated = input<string>();
+  updatedHeight = input<number | undefined | null>();
+
+  protected heightStyle = computed(() => {
+    return this.updatedHeight() ? `height: ${this.updatedHeight()}px;` : '';
+  });
 }
