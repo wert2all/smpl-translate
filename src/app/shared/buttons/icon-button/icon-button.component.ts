b48c72277d5c12d42878e8bc8895b8cdbbb8e5dc
Serhii Shvets <wert2all@gmail.com>
2025-08-13 11:52:21 +0300

    refactor: rename icon button component

--- src/app/shared/buttons/icon-button/icon-button.component.ts
+++ src/app/shared/buttons/icon-button/icon-button.component.ts
@@ -2,7 +2,7 @@ import { Component, input } from '@angular/core';
 import { NgIconComponent } from '@ng-icons/core';
 
 @Component({
-  selector: 'app-icon-button',
+  selector: 'app-shared-icon-button',
   templateUrl: './icon-button.component.html',
   imports: [NgIconComponent],
   styleUrls: ['./icon-button.component.scss'],
