221b9f1bf00ca960cb5578f6682cc0ca4d7aa5d9
Serhii Shvets <wert2all@gmail.com>
2025-08-13 20:55:53 +0300

    fix: fix spacer

--- src/app/shared/alert/alert.component.ts
+++ src/app/shared/alert/alert.component.ts
@@ -1,4 +1,4 @@
-import { Component, input } from '@angular/core';
+import { Component, computed, input } from '@angular/core';
 import { provideIcons } from '@ng-icons/core';
 import { phosphorWarningCircleLight } from '@ng-icons/phosphor-icons/light';
 
@@ -6,12 +6,19 @@ import { phosphorWarningCircleLight } from '@ng-icons/phosphor-icons/light';
   selector: 'app-shared-alert',
   imports: [],
   viewProviders: [provideIcons({ phosphorWarningCircleLight })],
-  template: `<div class="alert flex place-items-center border-1 p-4">
+  template: `<div
+    [class.error]="isError()"
+    [class.success]="isSuccess()"
+    class="alert flex place-items-center border-1 p-4">
     <span></span>
     {{ message() }}
   </div>`,
   styleUrl: './alert.component.scss',
 })
 export class AlertComponent {
+  type = input.required<'error' | 'success'>();
   message = input.required<string>();
+
+  protected isError = computed(() => this.type() === 'error');
+  protected isSuccess = computed(() => this.type() === 'success');
 }
