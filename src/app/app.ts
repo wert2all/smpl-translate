c033f90049e02b3252910c917a34ef08b8410c8e
Serhii Shvets <wert2all@gmail.com>
2025-08-15 01:00:00 +0300

    refactor: move user language settings to separate feature

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,19 +1,39 @@
-import { Component, HostListener, inject } from '@angular/core';
+import { Component, HostListener, inject, ViewChild } from '@angular/core';
 
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { filter } from 'rxjs';
 import { TranslateContainerComponent } from './features/translate-container/translate-container.component';
+import { UserLanguagesComponent } from './features/user-languages/user-languages.component';
+import { DialogsService } from './shared/services/dialogs.service';
+import { LanguageService } from './shared/services/language.service';
 import { MappingService } from './shared/services/mapping.service';
 import { ModeService } from './shared/services/mode.service';
-import { Mode } from './shared/shared.types';
+import { DialogType, LanguageCode, Mode } from './shared/shared.types';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
-  imports: [TranslateContainerComponent],
+  imports: [TranslateContainerComponent, UserLanguagesComponent],
 })
 export class App {
+  @ViewChild('settingUserLanguagesDialog')
+  settingUserLanguages!: UserLanguagesComponent;
+
   private modeService = inject(ModeService);
   private mappingService = inject(MappingService);
+  private languageService = inject(LanguageService);
+  private dialogService = inject(DialogsService);
 
+  constructor() {
+    this.dialogService.openWindow
+      .pipe(
+        takeUntilDestroyed(),
+        filter(type => type == DialogType.userLanguages)
+      )
+      .subscribe(() => {
+        this.showUserLanguages();
+      });
+  }
   @HostListener('document:keydown.escape')
   handleKeyboardEvent() {
     this.modeService.update(Mode.normal);
@@ -28,5 +48,13 @@ export class App {
   @HostListener('document:keydown', ['$event.key'])
   onSpacePress(key: string) {
     this.mappingService.pressKey(key);
+  }
+
+  protected saveUserLanguages(languages: LanguageCode[]) {
+    this.languageService.setUserLanguages(languages);
+  }
+
+  private showUserLanguages() {
+    this.settingUserLanguages.open();
   }
 }
--- src/app/features/translate-container/translate-container.component.ts
+++ src/app/features/translate-container/translate-container.component.ts
@@ -1,23 +1,19 @@
-import { Component, computed, inject, signal, ViewChild } from '@angular/core';
-import { takeUntilDestroyed, toSignal } from '@angular/core/rxjs-interop';
+import { Component, computed, inject, signal } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
 import { provideIcons } from '@ng-icons/core';
 import { flagGbSquare, flagUaSquare } from '@ng-icons/flag-icons/square';
-import { filter } from 'rxjs';
 import { AlertComponent } from '../../shared/components/alert/alert.component';
 import { LoaderComponent } from '../../shared/components/loader/loader.component';
 import { SpacerComponent } from '../../shared/components/spacer/spacer.component';
-import { DialogsService } from '../../shared/services/dialogs.service';
-import { LanguageService } from '../../shared/services/language.service';
 import { ModeService } from '../../shared/services/mode.service';
 import {
   createInitialState,
-  DialogType,
   LanguageCode,
   Mode,
   State,
 } from '../../shared/shared.types';
 import { BottomBarComponent } from './bottom-bar/bottom-bar.component';
-import { UserLanguagesComponent } from './dialogs/user-languages/user-languages.component';
+
 import { InputContainerComponent } from './input-container/input-container.component';
 import { LanguageSwitcherComponent } from './language-switcher/language-switcher.component';
 import { TranslationComponent } from './translation/translation.component';
@@ -34,17 +30,11 @@ import { TranslationComponent } from './translation/translation.component';
     InputContainerComponent,
     AlertComponent,
     LoaderComponent,
-    UserLanguagesComponent,
   ],
   viewProviders: [provideIcons({ flagUaSquare, flagGbSquare })],
 })
 export class TranslateContainerComponent {
-  @ViewChild('settingUserLanguagesDialog')
-  settingUserLanguages!: UserLanguagesComponent;
-
   private modeService = inject(ModeService);
-  private dialogService = inject(DialogsService);
-  private languageService = inject(LanguageService);
 
   protected inputString = signal('');
   protected height = signal<number | null>(null);
@@ -72,16 +62,6 @@ export class TranslateContainerComponent {
       : null;
   });
 
-  constructor() {
-    this.dialogService.openWindow
-      .pipe(
-        takeUntilDestroyed(),
-        filter(type => type == DialogType.userLanguages)
-      )
-      .subscribe(() => {
-        this.showUserLanguages();
-      });
-  }
   onFocus() {
     this.modeService.update(Mode.insert);
   }
@@ -92,13 +72,5 @@ export class TranslateContainerComponent {
 
   fired() {
     console.log('fired');
-  }
-
-  showUserLanguages() {
-    this.settingUserLanguages.open();
-  }
-
-  saveUserLanguages(languages: LanguageCode[]) {
-    this.languageService.setUserLanguages(languages);
   }
 }
