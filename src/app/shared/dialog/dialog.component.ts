921c72d2199a12a1bac6c908270ec6ae62cd33c1
Serhii Shvets <wert2all@gmail.com>
2025-08-14 16:40:31 +0300

    chore: remove usless import

--- src/app/shared/dialog/dialog.component.ts
+++ src/app/shared/dialog/dialog.component.ts
@@ -1,11 +1,9 @@
 import { Component, input } from '@angular/core';
-import { ButtonComponent } from '../buttons/button/button.component';
 
 @Component({
   selector: 'app-shared-dialog',
   templateUrl: './dialog.component.html',
   styleUrls: ['./dialog.component.scss'],
-  imports: [ButtonComponent],
 })
 export class DialogComponent {
   title = input.required<string>();
