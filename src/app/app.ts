d8c2ab472156b9eaa3a0d8a87ef5dad9f9730a10
Serhii Shvets <wert2all@gmail.com>
2025-08-19 11:06:00 +0300

    refactor: mode listerning of keybindings to bottom bar component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,7 @@
-import { Component, HostListener, inject, ViewChild } from '@angular/core';
+import { Component, inject, ViewChild } from '@angular/core';
 
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
-import { filter, Subject, withLatestFrom } from 'rxjs';
+import { filter } from 'rxjs';
 import { BottomBarComponent } from './features/bottom-bar/bottom-bar.component';
 import { LanguageSwitcherComponent } from './features/language-switcher/language-switcher.component';
 import { TranslateContainerComponent } from './features/translate-container/translate-container.component';
@@ -9,9 +9,7 @@ import { UserLanguagesComponent } from './features/user-languages/user-languages
 import { LayoutComponent } from './shared/components/layout/layout.component';
 import { ActionsService } from './shared/services/actions.service';
 import { LanguageService } from './shared/services/language.service';
-import { MappingService } from './shared/services/mapping.service';
-import { ModeService } from './shared/services/mode.service';
-import { Action, LanguageCode, Mode } from './shared/shared.types';
+import { Action, LanguageCode } from './shared/shared.types';
 
 @Component({
   selector: 'app-root',
@@ -28,12 +26,8 @@ export class App {
   @ViewChild('settingUserLanguagesDialog')
   settingUserLanguages!: UserLanguagesComponent;
 
-  private modeService = inject(ModeService);
-  private mappingService = inject(MappingService);
   private languageService = inject(LanguageService);
   private actionsService = inject(ActionsService);
-
-  private switchInsertMode = new Subject<void>();
 
   constructor() {
     this.actionsService.actions
@@ -44,35 +38,6 @@ export class App {
       .subscribe(() => {
         this.showUserLanguages();
       });
-
-    this.switchInsertMode
-      .pipe(takeUntilDestroyed(), withLatestFrom(this.modeService.mode))
-      .subscribe(([, mode]) => {
-        if (mode == Mode.normal) {
-          this.modeService.update(Mode.insert);
-        }
-      });
-  }
-
-  @HostListener('document:keydown.i')
-  handleIPress() {
-    this.switchInsertMode.next();
-  }
-
-  @HostListener('document:keydown.escape')
-  handleEscPress() {
-    this.modeService.update(Mode.normal);
-  }
-
-  @HostListener('document:keydown.control.s', ['$event'])
-  translate(e: Event) {
-    e.preventDefault();
-    this.actionsService.fireAction(Action.Translate);
-  }
-
-  @HostListener('document:keydown', ['$event.key'])
-  handleSpacePress(key: string) {
-    this.mappingService.pressKey(key);
   }
 
   protected saveUserLanguages(languages: LanguageCode[]) {
--- src/app/features/bottom-bar/bottom-bar.component.ts
+++ src/app/features/bottom-bar/bottom-bar.component.ts
@@ -1,10 +1,12 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, HostListener, inject, signal } from '@angular/core';
 import { takeUntilDestroyed, toSignal } from '@angular/core/rxjs-interop';
 
+import { Subject, withLatestFrom } from 'rxjs';
 import { KbdComponent } from '../../shared/components/kbd/kbd.component';
+import { ActionsService } from '../../shared/services/actions.service';
 import { MappingService } from '../../shared/services/mapping.service';
 import { ModeService } from '../../shared/services/mode.service';
-import { Mapping, Mode } from '../../shared/shared.types';
+import { Action, Mapping, Mode } from '../../shared/shared.types';
 import { ModeComponent } from '../translate-container/mode/mode.component';
 
 @Component({
@@ -14,18 +16,50 @@ import { ModeComponent } from '../translate-container/mode/mode.component';
 })
 export class BottomBarComponent {
   private modeService = inject(ModeService);
+  private mappingService = inject(MappingService);
+  private actionsService = inject(ActionsService);
 
   protected mode = toSignal(this.modeService.mode, {
     initialValue: Mode.insert,
   });
+  protected mappings = signal<Mapping[]>([]);
 
-  mappings = signal<Mapping[]>([]);
+  private switchInsertMode = new Subject<void>();
 
   constructor() {
-    inject(MappingService)
-      .activeMenu.pipe(takeUntilDestroyed())
+    this.mappingService.activeMenu
+      .pipe(takeUntilDestroyed())
       .subscribe(mappings => {
         this.mappings.set(mappings);
       });
+
+    this.switchInsertMode
+      .pipe(takeUntilDestroyed(), withLatestFrom(this.modeService.mode))
+      .subscribe(([, mode]) => {
+        if (mode == Mode.normal) {
+          this.modeService.update(Mode.insert);
+        }
+      });
+  }
+
+  @HostListener('document:keydown.control.s', ['$event'])
+  translate(e: Event) {
+    e.preventDefault();
+    this.actionsService.fireAction(Action.Translate);
+  }
+
+  @HostListener('document:keydown.i')
+  handleIPress() {
+    this.switchInsertMode.next();
+  }
+
+  @HostListener('document:keydown.escape')
+  handleEscPress() {
+    this.modeService.update(Mode.normal);
+  }
+
+  @HostListener('document:keydown', ['$event.key'])
+  handleSpacePress(key: string) {
+    this.mappingService.pressKey(key);
   }
 }
